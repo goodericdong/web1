beac6b7fce2c880d410c9cf8930d3ea72f96ec64
Eric Dong <ericdyj@gmail.com>
2020-07-23 17:23:15 -0400

    Intro slide and slide 01

--- static/PDF_generator.R
+++ static/PDF_generator.R
@@ -2,5 +2,11 @@
 
 # Run after modifying the .Rmd files
 
+## slide 0
+pagedown::chrome_print("static/slides/00-slide/00-slide.Rmd")
+
 ## slide 1
 pagedown::chrome_print("static/slides/01-slide/01-slide.Rmd")
+
+## slide 2
+pagedown::chrome_print("static/slides/02-slide/02-slide.Rmd")
