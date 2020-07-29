00c17efcb4e59323a3e3a0c903a2d4c7aede5adc
Eric Dong <ericdyj@gmail.com>
2020-07-29 16:57:37 -0400

    Case Guidelines added

--- R/build.R
+++ R/build.R
@@ -1,15 +1,14 @@
-# Render all the .Rmd files in static/slides
-blogdown::build_dir("static/slides")
+# Render all the .Rmd files in static/
+blogdown::build_dir("static/")
 
 
 # Convert all slide HTML files to PDF
-slide_names_with_ext <- list.files(here::here("static/slides/"), pattern = "*.html")
+slide_names_with_ext <- list.files(here::here("static/"), pattern = "*.Rmd", recursive = T)
 
 slide_names_sans_ext <- tools::file_path_sans_ext(slide_names_with_ext)
 
 pdfize <- function(slide_name) {
-  pagedown::chrome_print(here::here(paste0("static/slides/", slide_name, ".html")),
-                         output = here::here(paste0("static/slides/", slide_name, ".pdf")))
+  pagedown::chrome_print(here::here(paste0("static/", slide_name, ".Rmd")))
 }
 
 sapply(slide_names_sans_ext, pdfize)
